33959ae7064ed4ae301ef463d1187aa9e01a48bc
8-84 <100890206+8-84@users.noreply.github.com>
2023-11-16 00:20:29 +0900

    initial

--- src/main/java/mass_reg_lib/MassRegLib.java
+++ src/main/java/mass_reg_lib/MassRegLib.java
@@ -1,11 +1,13 @@
-package com.example.example_mod;
+package mass_reg_lib;
 
 import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.loader.api.ModDependency;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+import org.quiltmc.loader.api.QuiltLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
+public class MassRegLib implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod name as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
@@ -14,5 +16,6 @@ public class ExampleMod implements ModInitializer {
 	@Override
 	public void onInitialize(ModContainer mod) {
 		LOGGER.info("Hello Quilt world from {}!", mod.metadata().name());
+		QuiltLoader.getAllMods();
 	}
 }
--- src/main/java/mass_reg_lib/mixin/TitleScreenMixin.java
+++ src/main/java/mass_reg_lib/mixin/TitleScreenMixin.java
@@ -1,16 +1,17 @@
-package com.example.example_mod.mixin;
+package mass_reg_lib.mixin;
 
-import com.example.example_mod.ExampleMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import mass_reg_lib.MassRegLib;
+
 @Mixin(TitleScreen.class)
 public class TitleScreenMixin {
 	@Inject(method = "init", at = @At("TAIL"))
 	public void onInit(CallbackInfo ci) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		MassRegLib.LOGGER.info("This line is printed by an MassRegLib mod mixin!");
 	}
 }
